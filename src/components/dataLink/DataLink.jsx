45003b875284e26012b1bd44574745235c7213f8
Alexme116 <alexmoncada16@hotmail.com>
2024-04-28 11:26:40 -0600

    New user flow added

--- src/components/dataLink/DataLink.jsx
+++ src/components/dataLink/DataLink.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { fetchSpotifyApi } from "../../api/spotifyAPI";
+import { authFLow, getDataAuth } from '../../setup';
 import spotifyIcon from "../../assets/spotify-icon.png";
 import verifierIcon from "../../assets/verifier-icon.svg";
 import lockIcon from "../../assets/lock-icon.svg";
@@ -12,14 +13,20 @@ const DataLink = () => {
 
     const navigate = useNavigate()
 
-    const handleCodeVerifier = () => {
-        navigate('/register')
+    const handleCodeVerifier = async() => {
+        const codeChallengeProm = await getDataAuth()
+        authFLow(codeChallengeProm)
     }
 
     const handleToken = async () => {
         const token = window.localStorage.getItem('token')
+        const spotifyLink = document.getElementById("spotifyLink")
+        const syncDevice = document.getElementById("syncDevice")
         const lock = document.getElementById("tokenLock")
         lock.src = lockIcon
+        spotifyLink.style.display = "flex"
+        syncDevice.style.display = "flex"
+
         if (token == "undefined" || token == null) {
             const urlParams = new URLSearchParams(window.location.search);
             let code = urlParams.get('code');
@@ -46,6 +53,10 @@ const DataLink = () => {
 
             localStorage.setItem('token', response.access_token);
         }
+    }
+
+    const handleSpotifyLink = () => {
+        window.open('https://open.spotify.com/', '_blank')
     }
 
     const getDeviceId = async () => {
@@ -76,7 +87,7 @@ const DataLink = () => {
                 <div className="flex flex-col items-center m-14">
                     <div className="flex justify-center">
                         <img src={spotifyIcon} alt="spotify" width={"60px"}/>
-                        <h1 className="self-center ml-5 font-bold text-4xl">Data Link</h1>
+                        <h1 className="self-center ml-5 font-bold text-4xl">Spotify</h1>
                     </div>
                     <div className="flex flex-col items-center mt-10">
                         <button onClick={handleCodeVerifier}><img src={verifierIcon} alt="verifier" width={"50px"} /></button>
@@ -86,11 +97,11 @@ const DataLink = () => {
                         <button onClick={handleToken}><img id="tokenLock" src={lockedIcon} alt="lock" width={"50px"} /></button>
                         <h1 className="font-bold text-xl">Get Token</h1>
                     </div>
-                    <div className="flex flex-col items-center mt-5">
-                        <a href="https://open.spotify.com/" target="_blank"><img src={linkIcon} alt="link" width={"50px"} /></a>
+                    <div id="spotifyLink" className="hidden flex-col items-center mt-5">
+                        <button onClick={handleSpotifyLink}><img src={linkIcon} alt="link" width={"50px"} /></button>
                         <h1 className="font-bold text-xl">Open Spotify</h1>
                     </div>
-                    <div className="flex flex-col items-center mt-5">
+                    <div id="syncDevice" className="hidden flex-col items-center mt-5">
                         <button onClick={getDeviceId}><img src={syncIcon} alt="sync" width={"50px"} /></button>
                         <h1 className="font-bold text-xl">Sync Device</h1>
                     </div>
